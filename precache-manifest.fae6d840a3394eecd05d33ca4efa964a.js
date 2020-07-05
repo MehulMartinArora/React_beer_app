0ed1b58f1237b46084b7925c7f5848bd445e21c7
MehulMartinArora <mehul.18jics140@jietjodhpur.ac.in>
2020-07-05 18:12:46 +0530

    Updates

---
+++ precache-manifest.fae6d840a3394eecd05d33ca4efa964a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e5c4c0bc951f71fe46ec6cad041c67",
+    "url": "/react_beer_app/index.html"
+  },
+  {
+    "revision": "1d47fb46f82328393d81",
+    "url": "/react_beer_app/static/css/main.ae255e13.chunk.css"
+  },
+  {
+    "revision": "fbb303872551943d8726",
+    "url": "/react_beer_app/static/js/2.63c2d59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_beer_app/static/js/2.63c2d59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d47fb46f82328393d81",
+    "url": "/react_beer_app/static/js/main.ede0de62.chunk.js"
+  },
+  {
+    "revision": "400c9c6792d298591d65",
+    "url": "/react_beer_app/static/js/runtime-main.4a38f73d.js"
+  }
+]);
